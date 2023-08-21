4b7c2acc589df5c056fb020d6614ed971138183f
consultoria05 <consultoria05@hipstertechnology.com.br>
2023-08-21 19:30:57 -0300

    up

--- script.js
+++ script.js
@@ -18,6 +18,45 @@ window.addEventListener('load', () => {
     carregarTexto(arquivo);
 });
 
+// Conteúdo de divs laterais 
+
+// Suponha que você tenha uma lista de nomes de arquivo de texto
+const nomesDeArquivo = ["tituloTeste.txt", "contTeste.txt"];
+
+// Função para carregar e inserir o conteúdo dos arquivos de texto no HTML
+function carregarConteudo() {
+    const leiaTambemContainer = document.getElementById("leia-tambem-container");
+    
+    for (let i = 0; i < nomesDeArquivo.length; i++) {
+        const nomeDeArquivo = nomesDeArquivo[i];
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", nomeDeArquivo, true);
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const conteudo = xhr.responseText;
+                if (nomeDeArquivo.includes("titulo")) {
+                    // Se o nome do arquivo contém "titulo", insira o conteúdo no h1
+                    document.querySelector(".title-principal-post").textContent = conteudo;
+                } else if (nomeDeArquivo.includes("texto")) {
+                    // Se o nome do arquivo contém "texto", insira o conteúdo na seção "Leia também"
+                    const paragrafo = document.createElement("p");
+                    paragrafo.textContent = conteudo;
+                    leiaTambemContainer.appendChild(paragrafo);
+                }
+            }
+        };
+        
+        xhr.send();
+    }
+}
+
+// Chame a função para carregar o conteúdo dos arquivos de texto
+carregarConteudo();
+
+// Chame a função quando a página estiver carregada
+window.addEventListener('load', carregarConteudo);
+
 
 // Botão voltar 
 
