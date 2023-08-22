6d440fccf06e2f615584b37c1f49b3abaef137d3
consultoria05 <consultoria05@hipstertechnology.com.br>
2023-08-22 00:10:37 -0300

    up

--- nodeScript.js
+++ nodeScript.js
@@ -4,18 +4,18 @@ const app = express();
 
 app.get('/posts', (req, res) => {
     // Lê os diretórios e obtém os títulos e conteúdos dos posts
-    const titles = fs.readdirSync('titles');
-    const contents = fs.readdirSync('contents');
+    const titles = fs.readdirSync('blogOctoLegal\text');
+    const contents = fs.readdirSync('blogOctoLegal\contents');
 
     // Envie os dados como JSON
     const data = titles.map((title, index) => ({
         title,
-        content: fs.readFileSync(`contents/${contents[index]}`, 'utf-8')
+        content: fs.readFileSync(`blogOctoLegal\contents/${contents[index]}`, 'utf-8')
     }));
 
     res.json(data);
 });
 
-app.listen(3000, () => {
-    console.log('Servidor está rodando na porta 3000');
+app.listen(5000, () => {
+    console.log('Servidor está rodando na porta 5000');
 });
